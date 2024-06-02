9adc008c90bca8576994d55fbb1e8ca80a168ea6
Gabriel dos Santos Soares <gustavogusoares14@gmail.com>
2024-06-02 18:19:50 -0300

    Estilização

--- app/(tabs)/Logado.tsx
+++ app/(tabs)/Logado.tsx
@@ -1,12 +1,15 @@
 //Rotas pós logado tipo tabs
 
 import React from 'react';
-import {Feather} from '@expo/vector-icons';
+import { View } from 'react-native';
+import { Feather, Entypo } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Tela1 from './Tela1';
 import Tela2 from './Tela2';
 import Historico from './Historico';
+
+import { styles } from './css/css'
 
 const Tab = createBottomTabNavigator();
 
@@ -18,47 +21,53 @@ export default function Logado() {
         position:"absolute",
         backgroundColor:'#63c2d1',
         borderTopWidth:0,
-        // bottom:14,
-        // left:14,
-        // right:14,
+        bottom:14,
+        left:14,
+        right:14,
         elevation:0,
         borderRadius:7,
+        paddingBottom: 5,
+        paddingTop:5
         }
       }}
       
       >
-        <Tab.Screen
-          name="Tela1"
-          component={Tela1}
-          options={{
-              tabBarIcon:() => <Feather name='plus' color={"#fff"} size={30} />,
-              tabBarLabel:'Criar Escala',
-              headerShown: false,
-              tabBarActiveTintColor:"#fff",
-              tabBarInactiveTintColor:"#fff",
-          }}
-        />
         <Tab.Screen 
           name="Escalas"
           component={Tela2} 
           options={{ 
-            tabBarIcon:() => <Feather name='list' color={"#fff"} size={30}/>,
+            tabBarIcon:({size, color}) => <Feather name='list' color={color} size={size} />,
             tabBarBadge: "!" ,
             headerShown: false,
             tabBarActiveTintColor:"#fff",
-            tabBarInactiveTintColor:"#fff",
+            tabBarInactiveTintColor:"#808080",
           }} 
-          
+        />
+
+        <Tab.Screen
+          name="Tela1"
+          component={Tela1}
+          options={{
+            tabBarIcon:({size, color, focused}) => (
+              <View style={[styles.tabIcons, {backgroundColor: focused ? '#fff' : '#56A9B6'}]}>
+                <Entypo name='plus' color={focused ? '#56A9B6' : '#fff'} size={size} />
+              </View>
+            ),
+              tabBarLabel:'',
+              headerShown: false,
+              tabBarActiveTintColor:"#fff",
+              tabBarInactiveTintColor:"#808080",
+          }}
         />
 
         <Tab.Screen
           name='Histórico'
           component={Historico}
           options={{
-            tabBarIcon: () => <Feather name='book-open' color={"#fff"} size={30} />,
+            tabBarIcon:({size, color}) => <Feather name='book-open' color={color} size={size} />,
             headerShown: false,
             tabBarActiveTintColor:"#fff",
-            tabBarInactiveTintColor:"#fff",
+            tabBarInactiveTintColor:"#808080",
           }}  
         />
       </Tab.Navigator>
