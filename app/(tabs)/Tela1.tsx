66e397f67199a90289484900ec029e9b04c4f6bc
Gabriel dos Santos Soares <gustavogusoares14@gmail.com>
2024-06-05 20:11:37 -0300

    Botão de sair

--- app/(tabs)/Tela1.tsx
+++ app/(tabs)/Tela1.tsx
@@ -138,7 +138,7 @@ export default function Tela1({navigation}) {
   
   return (
     <View style={styles.container}>
-        <Feather name="home" style={{width:50, height:50, marginRight:'auto', bottom:95, right:-7}} color={'black'} size={50} onPress={()=> navigation.navigate("SingIn")}/>
+        <Feather name="home" style={{width:50, height:50, marginRight: 'auto', bottom:80, right:-20}} color={'#56A9B6'} size={30} onPress={()=> navigation.navigate("SingIn")}/>
         <Text style={styles.titleAgendamento2}>Criar Escala</Text>
         <Animatable.View delay={50} animation="fadeInUp">
         <Animatable.View delay={100} animation="fadeInUp">
