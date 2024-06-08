a2504b95672c5cb0542c8ee07dd2e48a81461df1
Gabriel dos Santos Soares <gustavogusoares14@gmail.com>
2024-06-08 12:54:53 -0300

    Adição da volta para o Login

--- app/(tabs)/SingUp.tsx
+++ app/(tabs)/SingUp.tsx
@@ -85,6 +85,9 @@ export default function SignUp() {
           >
           <Text style={styles.input3}>Cadastrar-se</Text>
         </TouchableOpacity>
+        <Text onPress={()=>navigation.navigate('SingIn' as never)} style={styles.texto}>
+            Já possui conta? Clique aqui para <Text style={styles.bold}>Logar-se!</Text>
+          </Text>
       </Animatable.View>
     </View>
   );
