66405b368089dc5069f8e567465d8623cf472570
Gabriel dos Santos Soares <gustavogusoares14@gmail.com>
2024-06-10 18:18:14 -0300

    Toques finais na estruturação

--- app/(tabs)/SingUp.tsx
+++ app/(tabs)/SingUp.tsx
@@ -1,17 +1,18 @@
+//ARQUIVO DA TELA DE CADASTRO
+
 import { Text } from 'react-native';
 import { useState } from 'react';
 import { View, Image, TextInput, TouchableOpacity, Alert , KeyboardAvoidingView, StatusBar} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { styles } from './css/css';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { getAuth } from 'firebase/auth';
-import { FIREBASE_APP } from '../../firebaseConfig';
-
 import {Ionicons} from '@expo/vector-icons';
+
+import { styles } from './css/css';
 
 import * as Animatable from 'react-native-animatable';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
+import { FIREBASE_APP } from '../../firebaseConfig';
 
 export default function SignUp() {
   const navigation = useNavigation();
@@ -19,16 +20,17 @@ export default function SignUp() {
   const [password, setPassword] = useState('');
   const auth = getAuth(FIREBASE_APP);
 
+  //Realizar cadastro
   async function handleSignUp() {
     if (email !== '' && password !== '') {
       try {
         const response = await createUserWithEmailAndPassword(auth, email, password);
-        Alert.alert('Bemvindo!', 'Conta criada com sucesso!');
+        Alert.alert('Bem-vindo!', 'Conta criada com sucesso!');
         await AsyncStorage.setItem('token', JSON.stringify(response.user));
         navigation.navigate('Logado' as never);
       } catch (error) {
         console.error('Deu ruim', error);
-        Alert.alert('Erro', 'Não foi possível criar a conta. Verifique os dados e tente novamente.');
+        Alert.alert('Erro!', 'Não foi possível criar a conta. Verifique os dados e tente novamente.');
       }
     } else {
       Alert.alert('Erro!','Preencha os campos de e-mail e senha.');
@@ -47,7 +49,6 @@ export default function SignUp() {
         source={require('../(tabs)/assets/imaculada.png')}
       />
       <Text style={{color:'#63c2d1',fontSize:18,}}>Bem vindo(a), Cadastre-se...</Text>
-
       <Animatable.View delay={1000} animation="fadeInUp" style={styles.container2}>
       <KeyboardAvoidingView behavior='position' enabled>
         <View style={styles.inputArea}>
@@ -61,17 +62,16 @@ export default function SignUp() {
           />
         </View>
         <View style={styles.inputArea}>
-        <Ionicons name="lock-closed" color="#fff" size={22} style={styles.iconLock}/>
-
-          <TextInput
-            placeholderTextColor="#fff"
-            onChangeText={(text) => setPassword(text)}
-            placeholder='Digite sua senha'
-            style={styles.input2}
-            value={password}
-            secureTextEntry={hidePass}
-          />
-          <TouchableOpacity style={styles.icon} onPress={()=> setHidePassa(!hidePass)}>
+          <Ionicons name="lock-closed" color="#fff" size={22} style={styles.iconLock}/>
+            <TextInput
+              placeholderTextColor="#fff"
+              onChangeText={(text) => setPassword(text)}
+              placeholder='Digite sua senha'
+              style={styles.input2}
+              value={password}
+              secureTextEntry={hidePass}
+            />
+            <TouchableOpacity style={styles.icon} onPress={()=> setHidePassa(!hidePass)}>
               {hidePass ?
               <Ionicons name="eye-off" color="#fff" size={22} />
               :
@@ -85,8 +85,8 @@ export default function SignUp() {
             onPress={handleSignUp}
           >
           <Text style={styles.input3}>Cadastrar-se</Text>
-        </TouchableOpacity>
-        <Text onPress={()=>navigation.navigate('SingIn' as never)} style={styles.texto}>
+          </TouchableOpacity>
+          <Text onPress={()=>navigation.navigate('SingIn' as never)} style={styles.texto}>
             Já possui conta? Clique aqui para <Text style={styles.bold}>Logar-se!</Text>
           </Text>
       </Animatable.View>
