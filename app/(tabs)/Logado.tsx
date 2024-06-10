73548d7a5ebcaf2240308a61eeb047f183d84451
Gabriel dos Santos Soares <gustavogusoares14@gmail.com>
2024-06-10 18:17:48 -0300

    Toques finais na estruturação

--- app/(tabs)/Logado.tsx
+++ app/(tabs)/Logado.tsx
@@ -1,4 +1,4 @@
-//Rotas pós logado tipo tabs
+//ARQUIVO DE ROTAS PÓS LOGIN DO TIPO TABS
 
 import React from 'react';
 import { View } from 'react-native';
@@ -15,63 +15,60 @@ const Tab = createBottomTabNavigator();
 
 export default function Logado() {
   return (
-      <Tab.Navigator
+    <Tab.Navigator
       screenOptions={{headerShown:true,
-      tabBarStyle:{
-        position:"absolute",
-        backgroundColor:'#63c2d1',
-        borderTopWidth:0,
-        bottom:14,
-        left:14,
-        right:14,
-        elevation:0,
-        borderRadius:7,
-        paddingBottom: 5,
-        paddingTop:5
+        tabBarStyle:{
+          position:"absolute",
+          backgroundColor:'#63c2d1',
+          borderTopWidth:0,
+          bottom:14,
+          left:14,
+          right:14,
+          elevation:0,
+          borderRadius:7,
+          paddingBottom: 5,
+          paddingTop:5
         }
       }}
-      
-      >
-        <Tab.Screen 
-          name="Escalas"
-          component={Tela2} 
-          options={{ 
-            tabBarIcon:({size, color}) => <Feather name='list' color={color} size={size} />,
+    >
+      <Tab.Screen 
+        name="Escalas"
+        component={Tela2} 
+        options={{ 
+          tabBarIcon:({size, color}) => <Feather name='list' color={color} size={size} />,
             tabBarBadge: "!" ,
             headerShown: false,
             tabBarActiveTintColor:"#fff",
             tabBarInactiveTintColor:"#808080",
-          }} 
-        />
+        }} 
+      />
 
-        <Tab.Screen
-          name="Tela1"
-          component={Tela1}
-          options={{
-            tabBarIcon:({size, color, focused}) => (
-              <View style={[styles.tabIcons, {backgroundColor: focused ? '#fff' : '#56A9B6'}]}>
-                <Entypo name='plus' color={focused ? '#56A9B6' : '#fff'} size={size} />
-              </View>
-            ),
-              tabBarLabel:'',
-              headerShown: false,
-              tabBarActiveTintColor:"#fff",
-              tabBarInactiveTintColor:"#808080",
-          }}
-        />
-
-        <Tab.Screen
-          name='Histórico'
-          component={Historico}
-          options={{
-            tabBarIcon:({size, color}) => <Feather name='book-open' color={color} size={size} />,
+      <Tab.Screen
+        name="Tela1"
+        component={Tela1}
+        options={{
+          tabBarIcon:({size, focused}) => (
+            <View style={[styles.tabIcons, {backgroundColor: focused ? '#fff' : '#56A9B6'}]}>
+              <Entypo name='plus' color={focused ? '#56A9B6' : '#fff'} size={size} />
+            </View>
+          ),
+            tabBarLabel:'',
             headerShown: false,
             tabBarActiveTintColor:"#fff",
             tabBarInactiveTintColor:"#808080",
-          }}  
-        />
-      </Tab.Navigator>
+        }}
+      />
+
+      <Tab.Screen
+        name='Histórico'
+        component={Historico}
+        options={{
+          tabBarIcon:({size, color}) => <Feather name='book-open' color={color} size={size} />,
+            headerShown: false,
+            tabBarActiveTintColor:"#fff",
+            tabBarInactiveTintColor:"#808080",
+        }}  
+      />
+    </Tab.Navigator>
   );
 }
-
-
